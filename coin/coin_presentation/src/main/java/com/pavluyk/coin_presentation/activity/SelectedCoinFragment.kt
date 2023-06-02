992ee5193fc4c5e83d323d7d0d502d39d841997d
Andrey <andworked@gmail.com>
2023-06-02 17:13:26 +0300

    added fragment

---
+++ coin/coin_presentation/src/main/java/com/pavluyk/coin_presentation/activity/SelectedCoinFragment.kt
@@ -0,0 +1,25 @@
+package com.pavluyk.coin_presentation.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pavluyk.coin_presentation.R
+
+class SelectedCoinFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selected_coin, container, false)
+    }
+
+}
